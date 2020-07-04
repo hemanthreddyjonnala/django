a35531d84886899be6bc68652c5fac7f8fc69f46
hemanthreddyjonnala <hemanthreddyjonnala@gmail.com>
2020-07-04 09:10:05 -0400

    contact page

--- contact/views.py
+++ contact/views.py
@@ -2,11 +2,6 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import ContactForm
-import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-
-
 
 def contactView(request):
     if request.method == 'GET':
@@ -17,27 +12,12 @@ def contactView(request):
             subject = form.cleaned_data['subject']
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
-            # try:
-            #     send_mail(subject, message, from_email, ['jonnalapycreations@gmail.com'])
-            # except BadHeaderError:
-            #     return HttpResponse('Invalid header found.')
-            # return redirect('success')
-
-            message = Mail(
-                from_email=from_email,
-                to_emails='jonnalapycreations@gmail.com',
-                subject=subject,
-                html_content=message)
             try:
-                sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-                print(sg)
-                response = sg.send(message)
-                print(response.status_code)
-                print(response.body)
-                print(response.headers)
-            except Exception as e:
-                return HttpResponse(e)
+                send_mail(subject, message, from_email, ['jonnalapycreations@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
             return redirect('success')
+
     return render(request, "contact/email.html", {'form': form})
 
 
