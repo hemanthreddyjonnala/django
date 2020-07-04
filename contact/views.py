6afe85aa6a8270273c607fbf9c9b1632dd2c0af0
hemanthreddyjonnala <hemanthreddyjonnala@gmail.com>
2020-07-04 10:32:44 -0400

    contact page2

--- contact/views.py
+++ contact/views.py
@@ -4,21 +4,22 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 
 def contactView(request):
-    if request.method == 'GET':
-        form = ContactForm()
-    else:
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = form.cleaned_data['subject']
-            from_email = form.cleaned_data['from_email']
-            message = form.cleaned_data['message']
-            try:
-                send_mail(subject, message, from_email, ['jonnalapycreations@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('success')
+#     if request.method == 'GET':
+#         form = ContactForm()
+#     else:
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             subject = form.cleaned_data['subject']
+#             from_email = form.cleaned_data['from_email']
+#             message = form.cleaned_data['message']
+#             try:
+#                 send_mail(subject, message, from_email, ['jonnalapycreations@gmail.com'])
+#             except BadHeaderError:
+#                 return HttpResponse('Invalid header found.')
+#             return redirect('success')
 
-    return render(request, "contact/email.html", {'form': form})
+    # return render(request, "contact/email.html", {'form': form})
+    return render(request, "contact/email.html")
 
 
 def successView(request):
